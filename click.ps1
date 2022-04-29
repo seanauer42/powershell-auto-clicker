8bacf23636e28061d99db47ab18573445cd43d57
seanauer42 <44852194+seanauer42@users.noreply.github.com>
2022-04-29 04:41:01 -0400

    Create click.ps1
    This should be the final version because it has all of the functionality that I need (clicking every 0.6 seconds until a force quit).

---
+++ click.ps1
@@ -0,0 +1,8 @@
+#import mouse_event
+Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
+#left mouse click
+while($true)
+{
+	[W.U32]::mouse_event(6,0,0,0,0);
+	Start-Sleep -s 0.6;
+}
