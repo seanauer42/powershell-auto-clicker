ae7659ad7756ba9f985965de687e70a6650349d8
seanauer42 <44852194+seanauer42@users.noreply.github.com>
2022-12-12 09:14:16 -0500

    Update click.ps1
    added a stop when the foreground window loses focus

--- click.ps1
+++ click.ps1
@@ -1,8 +1,27 @@
 #import mouse_event
 Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
-#left mouse click
+
+# Let's get the foreground window so we know when it closes
+Add-Type @"
+  using System;
+  using System.Runtime.InteropServices;
+  public class Tricks {
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+}
+"@
+
+$a = [tricks]::GetForegroundWindow()
+
+#left mouse clicks until the window loses focus
 while($true)
 {
-	[W.U32]::mouse_event(6,0,0,0,0);
-	Start-Sleep -s 0.6;
+	$b = [tricks]::GetForegroundWindow()
+	if ($b -eq $a) {
+		[W.U32]::mouse_event(6,0,0,0,0);
+		Start-Sleep -s 1.0;
+	}
+	else {
+		break;
+	}
 }
